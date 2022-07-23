bb68dc44fcc7b48f54899b0e22da8927a6ecfdb2
Amanda Liem <amandaliem94@gmail.com>
2022-07-22 22:58:59 -0500

    added comments

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ import discord
 import os
 import random
 
+###############################################################################
+# IDs
+###############################################################################
+# Found by toggling developer mode on your raspberry pi
+# They are recommended when identifying a user, since people
+# can change their usernames/nicknames
 amaid=206298476802342912
 jenid=250073550478639104
 
 client = discord.Client()
 
+###############################################################################
+# Create a list of quotes from fortunes.txt
+###############################################################################
 fortunes = open("/home/pi/git/levi-bot/fortunes.txt", "r").readlines()
 
+###############################################################################
+# List of greetings
+###############################################################################
 greetings = ["Lol nerd",
         "Who are you",
         "Aloha",
@@ -22,18 +34,29 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    # Ignore the message if we're the one that sent it
     if message.author == client.user:
         return
 
+    ###########################################################################
+    # Greeting command
+    ###########################################################################
     if message.content.startswith('$hello'):
         if message.author.id == jenid:
             await message.channel.send("Hello Mistress")
         else:
             await message.channel.send(greetings[random.randint(0,len(greetings)-1)])
 
+    ###########################################################################
+    # Fortune command
+    ###########################################################################
     if message.content.startswith('$fortune'):
         await message.channel.send(fortunes[random.randint(0,len(fortunes)-1)])
 
+    ###########################################################################
+    # Infraction command
+    ###########################################################################
     if message.content.startswith('$infraction'):
         if message.author.id != jenid:
             await message.channel.send("I don't take orders from you.")
@@ -41,7 +64,5 @@ async def on_message(message):
         taggedUser = message.mentions[0];
         await message.channel.send('infracting ' + taggedUser.name)
         
-
 token=open("/home/pi/git/levi-bot/token.txt").readline().rstrip()
 client.run(token)
-
