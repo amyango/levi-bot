9e1532905153877d5a12d793bc7130ee663633d8
Amanda Liem <amandaliem94@gmail.com>
2022-08-04 00:17:11 -0500

    Adding $tothefarm, $berry, and $slots

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ from datetime import datetime
 from json.decoder import JSONDecodeError
 import pokebase as pb
 
+# Function to print log messages
 def log(msg):
     print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' ' + msg, flush=True)
+
+# Safe open json file
+def openjson(filename):
+    try:
+        result = json.load(open(filename, 'r'))
+    except JSONDecodeError:
+        result = {}
+    return result
 
 ###############################################################################
 # IDs
@@ -20,171 +29,45 @@ def log(msg):
 amaid=206298476802342912
 jenid=250073550478639104
 
+###############################################################################
+# Discord Initialization
+###############################################################################
+# Setting intents will pre-load some discord information
+# The members and reaction initializations are added so that the reaction
+# listeners will work.
 intents = discord.Intents.default()
 intents.members = True
 intents.reactions = True
-
 client = discord.Client(intents=intents)
 
-###############################################################################
-# Create a list of quotes from fortunes.txt
-###############################################################################
-fortunes = open("/home/pi/git/levi-bot/fortunes.txt", "r").readlines()
+usage='Commands: $hello $fortune $infraction $roll $tothefarm $slots $points'
 
 ###############################################################################
-# List of greetings
+# Opening Files
 ###############################################################################
-greetings = ["Lol nerd",
-        "Who are you",
-        "Aloha",
-        "Commencing ban",
-        "hi"
-        ]
+projectdir = '/home/pi/git/levi-bot/'
+datadir = projectdir + 'data/'
 
-###############################################################################
-# infractions list
-###############################################################################
-projectdir = "/home/pi/git/levi-bot/"
-infractfile = projectdir + "data/infractions.json"
-pokefile = projectdir + "data/pokemon.json"
-pointsfile = projectdir + "data/points.json"
-infractions = {}
-try: # Add try-catch in case the infractions.json file is empty
-    infractions = json.load(open(infractfile, 'r'))
-except JSONDecodeError:
-    pass
+fortunefile = projectdir + "fortunes.txt"
+greetingfile = projectdir + "greetings.txt"
 
-pokemon = {}
-try: # Add try-catch in case the infractions.json file is empty
-    pokemon = json.load(open(pokefile, 'r'))
-except JSONDecodeError:
-    pass
+infractfile = datadir + "infractions.json"
+pokefile = datadir + "pokemon.json"
+berryfile = datadir + "berries.json"
+poketeamsfile = datadir + "poketeams.json"
+pointsfile = datadir + "points.json"
 
-points = {}
-try: # Add try-catch in case the infractions.json file is empty
-    points = json.load(open(pointsfile, 'r'))
-except JSONDecodeError:
-    pass
+fortunes = open(fortunefile, "r").readlines()
+greetings = open(greetingfile, "r").readlines()
+infractions = openjson(infractfile)
+pokemon = openjson(pokefile)
+poketeams = openjson(poketeamsfile)
+points = openjson(pointsfile)
+berries = openjson(berryfile)
 
 ###############################################################################
-# bingo command
+# Pokemon
 ###############################################################################
-
-# Readonly variables
-x = ':x:'
-o = ':green_circle:'
-usage = 'Usage: $bingo <board/respin/list/stamp #>'
-
-# Global variables
-bingo = {}
-bingo_stat = {}
-bingo_items = open(projectdir + "bingo.txt").readlines()
-
-# Initialize Stamps
-for item in bingo_items:
-    bingo_stat[item] = x
-
-# Function that prints the list of stamps
-async def bingo_list(message):
-    bingo_prt = ''
-    for item in bingo_items:
-        bingo_prt = bingo_prt + bingo_stat[item] + item
-    await message.channel.send(bingo_prt)
-
-# Main function for the $bingo command
-async def dobingo(message):
-    global bingo_stat
-    words = message.content.split(" ")
-    subcmd = ""
-
-    if len(words) > 1:
-        subcmd = words[1]
-
-    if subcmd == "board":
-        await bingo_init(message, False)
-    elif subcmd == "respin":
-        await bingo_init(message, True)
-    elif subcmd == "list":
-        await bingo_list(message)
-    elif subcmd == "stamp":
-        await bingo_stamp(message, words)
-    elif subcmd == "clear":
-        await bingo_clear(message)
-    else:
-        await message.channel.send(usage)
-
-# Stamp an item off the list
-async def bingo_stamp(message, words):
-    if len(words) >= 3:
-
-        try:
-            idx = int(words[2]) - 1
-        except ValueError:
-            await message.channel.send("NOT A NUMBER.\n" + usage)
-            return
-
-        if idx > 23 or idx < 0:
-            await message.channel.send("Number out of range.\n" + usage)
-            return
-
-        bingo_stat[bingo_items[idx]] = o
-        await message.channel.send(bingo_stat[bingo_items[idx]] + bingo_items[idx])
-    else:
-        await message.channel.send("NO NUMBER PROVIDED\n" + usage)
-
-# Clear the list of stamps
-async def bingo_clear(message):
-        if message.author.id != jenid and message.author.id != amaid:
-            await message.channel.send("I don't take orders from you.")
-            return
-        for item in bingo_items:
-            bingo_stat[item] = x
-        await message.channel.send("Stamps have been cleaned.")
-
-# Create a board or show a board
-async def bingo_init(message, new):
-    if new or not message.author.id in bingo:
-        await message.channel.send("Generating board for " + message.author.name)
-        # Fill out the bingo board with 1-24 randomly
-        numbers = []
-        for i in range(1, 25):
-            numbers.append(i)
-        random.shuffle(numbers)
-        numbers.insert(12, 0)
-
-        print(numbers)
-
-        bingo[message.author.id] = numbers
-
-    await bingo_board_print(message)
-
-# Print the user's board
-# Each 'space' is a 2x2 grid of emojis:
-#   :one: :four:   for     1 4
-#   :x:   :x:              x x
-async def bingo_board_print(message):
-    output = ''
-
-    numbers = [':zero:', ':one:', ':two:', ':three:', ':four:',
-               ':five:', ':six:', ':seven:', ':eight:', ':nine:']
-
-    statstring = ''
-    count = 0
-    for space in bingo[message.author.id]:
-        if space == 0:
-            stat = o
-        else:
-            stat = bingo_stat[bingo_items[space-1]]
-
-        output = output + numbers[space // 10] + numbers[space % 10] + ' '
-        statstring = statstring + stat + stat + ' '
-        if (count + 1) % 5 == 0:
-            output = output + '\n'
-            output = output + statstring + '\n'
-            statstring = ''
-        count += 1
-
-    await message.channel.send(output)
 
 ###############################################################################
 # main
@@ -197,38 +80,47 @@ async def on_ready():
 async def on_reaction_add(reaction, user):
     log('reaction on message by ' + reaction.message.author.name)
 
+    # Levi doesn't care about points
     if reaction.message.author == client.user:
         return
 
-    add_points(reaction.message.author.id, 1)
+    # Don't let people give themselves points
+    # Cuz that's sad
+    if reaction.message.author == user:
+        return
 
-    if reaction.emoji == '⬅':
-        if reaction.message.id in menus:
-            log("message")
-    elif reaction.emoji == '➡':
-        if reaction.message.id in menus:
-            log("message")
+    add_points(reaction.message.author.id, 1)
 
 @client.event
 async def on_reaction_remove(reaction, user):
     log('reaction removed on message by ' + reaction.message.author.name)
     add_points(reaction.message.author.id, -1)
 
+async def do_greeting(message):
+    if message.author.id == jenid:
+        await message.channel.send("Hello Mistress")
+    else:
+        await message.channel.send(greetings[random.randint(0,len(greetings)-1)])
+
 @client.event
 async def on_message(message):
-
-    # Ignore the message if we're the one that sent it
+# Levi doesn't want to talk to himself # That's lame
     if message.author == client.user:
+        return
+
+    ###########################################################################
+    # Help command
+    ###########################################################################
+    if message.content.startswith('$help'):
+        await message.channel.send(usage)
         return
 
     ###########################################################################
     # Greeting command
     ###########################################################################
     if message.content.startswith('$hello'):
-        if message.author.id == jenid:
-            await message.channel.send("Hello Mistress")
-        else:
-            await message.channel.send(greetings[random.randint(0,len(greetings)-1)])
+        do_greeting(message)
+        return
 
     ###########################################################################
     # Fortune command
@@ -269,20 +161,18 @@ async def on_message(message):
         await message.channel.send(msg)
 
     ###########################################################################
-    # Bingo
-    ###########################################################################
-    if message.content.startswith('$bingo'):
-        await dobingo(message)
-
-    ###########################################################################
     # Gatcha
     ###########################################################################
     if message.content.startswith('$roll'):
 
-        team = get_pokemon(message.author.id)
+        team = get_poketeam(message.author.id)
 
         if len(team) == 0:
             newpokeid = random.randint(1, 898)
+        elif len(team) >= 6:
+            await message.channel.send('You already have 6 pokemon you greedy bitch')
+            await message.channel.send('hint: $tothefarm <pokemon> to release')
+            return
         else:
             pts = get_points(message.author.id)
             if pts < 10:
@@ -293,14 +183,17 @@ async def on_message(message):
 
         await message.channel.send(':game_die: R O L L I N G :game_die:')
 
-        newpoke = pb.pokemon(newpokeid)
-        team.append(newpokeid)
-        set_pokemon(message.author.id, team)
+        newpoke = {}
+        newpoke['id'] = newpokeid
+        newpoke['name'] = pb.pokemon(newpokeid).name
+        newpoke['basepic'] = pb.SpriteResource('pokemon', newpokeid).url
+        team.append(newpoke)
+        set_poketeam(message.author.id, team)
 
-        log(message.author.name + ' new pokemon ' + newpoke.name)
+        log(message.author.name + ' new pokemon ' + newpoke['name'])
 
-        await message.channel.send('Welcome ' + newpoke.name + ' to your team!')
-        await message.channel.send(pb.SpriteResource('pokemon', newpokeid).url)
+        await message.channel.send('Welcome ' + newpoke['name'] + ' to your team!')
+        await message.channel.send(newpoke['basepic'])
 
     if message.content.startswith('$team'):
 
@@ -309,7 +202,7 @@ async def on_message(message):
         else:
             user = message.author
 
-        team = get_pokemon(user.id)
+        team = get_poketeam(user.id)
 
         i = 0
         secs = 0
@@ -320,13 +213,108 @@ async def on_message(message):
         #await message.add_reaction('⬅')
         #await message.add_reaction('➡')
 
+    if message.content.startswith('$tothefarm'):
+        words = message.content.split(" ")
+        if len(words) > 1:
+            # look for the pokemon send to the farm
+            team = get_poketeam(message.author.id)
+
+            if words[1] == "allthembitches":
+                await message.channel.send('Goodbye bitches!')
+                nberries = 0
+                for i in range(len(team)):
+                    nberries += random.randint(1, 10)
+                    await message.channel.send(team[i]['basepic'])
+                team = []
+                set_poketeam(message.author.id, team)
+                return
+
+            for i in range(len(team)):
+                if team[i]['name'] == words[1]:
+                    nberries = random.randint(1, 10)
+                    await message.channel.send('Goodbye ' + team[i]['name'] + '!')
+                    await message.channel.send('The farm sent you back ' + str(nberries) + ' ~~soylent chunks~~ berries')
+                    del team[i]
+                    add_berries(message.author.id, nberries)
+                    set_poketeam(message.author.id, team)
+                    return
+        await message.channel.send('Usage: $tothefarm <pokemon name/allthembitches>') 
+
+    if message.content.startswith('$berry'):
+        nberries = get_berries(message.author.id)
+        msg="" + message.author.name + " berries: " + str(nberries)
+        await message.channel.send(msg)
+
+    if message.content.startswith('$slots'):
+
+        pts = get_points(message.author.id)
+
+        if pts < 1:
+            await message.channel.send('You have insufficient points (1 per spin): ' + str(pts))
+            return
+
+        await message.channel.send(':game_die: S L O T S :game_die:')
+
+
+def pull_slot(message):
+    icons = [':fire:', ':leaves:', ':ocean:', ':cherries:', ':coin:']
+    ncols = 3
+    result = [None] * ncols
+    pts = -1
+
+    string = ''
+
+    for i in range(0, ncols):
+        result[i] = random.randint(0, len(icons)-1)
+        string = string + icons[result[i]]
+    await message.channel.send(string)
+
+    max_icon = 0
+    max_matches = 1
+
+    for i in range(0, len(icons)):
+        if result[i] == i:
+
+    string = ''
+    if (result[0] == result[1]) and (result[0] == result[2]):
+        pts = (result[0] + 1) * 10
+        string = string + '\n    J A C K P O T    \n***' + str(pts) + '***'
+    elif result[0] == result[1] or result[0] == result[2]: 
+        pts = ((result[0] + 1) // 2) + 1
+        string = string + '\nSmall Winner!\n***' + str(pts) + '***'
+    elif result[1] == result[2]:
+        pts = ((result[1] + 1) // 2) + 1
+        string = string + '\nSmall Winner!\n***' + str(pts) + '***'
+    else:
+        string = string + '\n***' + 'Try again' + '***' + ' :grimacing:'
+
+    await message.channel.send(string)
+    add_points(message.author.id, pts)
+
+    
+
 def poke_embed(pokemon):
-    entry = pb.pokemon(pokemon)
-    embed = discord.Embed(title=entry.name.capitalize())
-    sprite = pb.SpriteResource('pokemon', pokemon)
-    embed.set_thumbnail(url=sprite.url)
+    embed = discord.Embed(title=pokemon['name'].capitalize())
+    embed.set_thumbnail(url=pokemon['basepic'])
+    if not 'lvl' in pokemon:
+        pokemon['lvl'] = 1
+    embed.add_field(name='Level', value=pokemon['lvl'], inline=True)
     return embed
 
+
+def get_berries(uid):
+    if not str(uid) in berries:
+        berries[str(uid)] = 0
+    return berries[str(uid)]
+
+def add_berries(uid, pts):
+    if not str(uid) in berries:
+        berries[str(uid)] = 0
+    else:
+        berries[str(uid)] += pts
+
+    with open(berryfile, 'w') as outfile:
+        json.dump(berries, outfile)
 
 def get_points(uid):
     if not str(uid) in points:
@@ -344,15 +332,25 @@ def add_points(uid, pts):
 
     return points[str(uid)]
 
-def set_pokemon(uid, team):
-    pokemon[str(uid)] = team
-    with open(pokefile, 'w') as outfile:
-        json.dump(pokemon, outfile)
+#def set_pokemon(uid, team):
+#    pokemon[str(uid)] = team
+#    with open(pokefile, 'w') as outfile:
+#        json.dump(pokemon, outfile)
     
-def get_pokemon(uid):
-    if not str(uid) in pokemon:
-        pokemon[str(uid)] = []
-    return pokemon[str(uid)]
-        
+#def get_pokemon(uid):
+#    if not str(uid) in pokemon:
+#        pokemon[str(uid)] = []
+#    return pokemon[str(uid)]
+
+def get_poketeam(uid):
+    if not str(uid) in poketeams:
+        poketeams[str(uid)] = []
+    return poketeams[str(uid)]
+
+def set_poketeam(uid, team):
+    poketeams[str(uid)] = team
+    with open(poketeamsfile, 'w') as outfile:
+        json.dump(poketeams, outfile)
+ 
 token=open("/home/pi/git/levi-bot/token.txt").readline().rstrip()
 client.run(token)
