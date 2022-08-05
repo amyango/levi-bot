8fdb4ab61f15ad1a3d31a73cdf1084fb65401f7c
Amanda Liem <amandaliem94@gmail.com>
2022-08-05 11:42:24 -0500

    Refactor

--- main.py
+++ main.py
@@ -78,8 +78,6 @@ async def on_ready():
 
 @client.event
 async def on_reaction_add(reaction, user):
-    log('reaction on message by ' + reaction.message.author.name)
-
     # Levi doesn't care about points
     if reaction.message.author == client.user:
         return
@@ -93,18 +91,149 @@ async def on_reaction_add(reaction, user):
 
 @client.event
 async def on_reaction_remove(reaction, user):
-    log('reaction removed on message by ' + reaction.message.author.name)
     add_points(reaction.message.author.id, -1)
 
+###############################################################################
+# $hello
+###############################################################################
 async def do_greeting(message):
     if message.author.id == jenid:
         await message.channel.send("Hello Mistress")
     else:
         await message.channel.send(greetings[random.randint(0,len(greetings)-1)])
 
+###############################################################################
+# $infraction
+###############################################################################
+async def do_infraction(message):
+
+    # Only Jen can use this function
+    if message.author.id != jenid:
+        await message.channel.send("I don't take orders from you.")
+        return
+
+    taggedUser = message.mentions[0]
+
+    if str(taggedUser.id) in infractions:
+        infractions[str(taggedUser.id)] += 1
+    else:
+        infractions[str(taggedUser.id)] = 1
+
+    with open(infractfile, 'w') as outfile:
+        json.dump(infractions, outfile)
+
+    msg="" + taggedUser.name + " infractions: " + str(infractions[str(taggedUser.id)])
+    await message.channel.send(msg)
+
+###############################################################################
+# $roll
+###############################################################################
+async def do_pokeroll(message):
+
+    # Messages
+    greedybitch='You already have 6 pokemon you greedy bitch\n'
+    greedybitch+='hint: $tothefarm <pokemon> to release'
+    poorbitch='You have insufficient points (10 per roll): '
+
+    team = get_poketeam(message.author.id)
+
+    # Get a free pokemon if team is empty
+    if len(team) == 0:
+        newpokeid = random.randint(1, 898)
+    elif len(team) >= 6:
+        await message.channel.send(greedybitch)
+        return
+    else:
+        pts = get_points(message.author.id)
+        if pts < 10:
+            await message.channel.send(poorbitch + str(pts))
+            return
+        add_points(message.author.id, -10)
+        newpokeid = random.randint(1, 898)
+
+    await message.channel.send(':game_die: R O L L I N G :game_die:')
+
+    newpoke = newpokemon(newpokeid)
+    team.append(newpoke)
+    set_poketeam(message.author.id, team)
+
+    log(message.author.name + ' new pokemon ' + newpoke['name'])
+
+    await message.channel.send('Welcome ' + newpoke['name'] + ' to your team!')
+    await message.channel.send(newpoke['basepic'])
+
+###############################################################################
+# Create new pokemon
+###############################################################################
+def newpokemon(pid):
+    newpokemon = {}
+    newpokemon['id'] = pid
+    newpokemon['name'] = pb.pokemon(pid).name
+    newpokemon['basepic'] = pb.SpriteResource('pokemon', pid).url
+    return newpokemon
+
+###############################################################################
+# $roll
+###############################################################################
+async def do_poketeam(message):
+    if len(message.mentions) > 0:
+        user = message.mentions[0];
+    else:
+        user = message.author
+
+    team = get_poketeam(user.id)
+
+    i = 0
+    secs = 0
+
+    for pokemon in team:
+        embed = poke_embed(pokemon)
+        await message.channel.send(embed=embed)
+    #await message.add_reaction('⬅')
+    #await message.add_reaction('➡')
+
+###############################################################################
+# $tothefarm <pokemon/allthembitches>
+###############################################################################
+async def do_murder(message):
+
+    murderusage='Usage: $tothefarm <pokemon name/allthembitches>'
+
+    words = message.content.split(" ")
+
+    # Argument checking
+    if len(words) < 2:
+        await message.channel.send(murderusage)
+        return
+
+    # Look for the pokemon send to the farm
+    team = get_poketeam(message.author.id)
+
+    if words[1] == "allthembitches":
+        await message.channel.send('Goodbye bitches!')
+        nberries = 0
+        for i in range(len(team)):
+            nberries += random.randint(1, 10)
+            await message.channel.send(team[i]['basepic'])
+        team = []
+        set_poketeam(message.author.id, team)
+        return
+
+    for i in range(len(team)):
+        if team[i]['name'] == words[1]:
+            nberries = random.randint(1, 10)
+            await message.channel.send('Goodbye ' + team[i]['name'] + '!')
+            await message.channel.send(team[i]['basepic'])
+            await message.channel.send('The farm sent you back ' + str(nberries) + ' ~~soylent chunks~~ berries')
+            del team[i]
+            add_berries(message.author.id, nberries)
+            set_poketeam(message.author.id, team)
+            return
+
 @client.event
 async def on_message(message):
-# Levi doesn't want to talk to himself # That's lame
+
+    # Levi doesn't want to talk to himself # That's lame
     if message.author == client.user:
         return
 
@@ -119,7 +248,7 @@ async def on_message(message):
     # Greeting command
     ###########################################################################
     if message.content.startswith('$hello'):
-        do_greeting(message)
+        await do_greeting(message)
         return
 
     ###########################################################################
@@ -127,142 +256,82 @@ async def on_message(message):
     ###########################################################################
     if message.content.startswith('$fortune'):
         await message.channel.send(fortunes[random.randint(0,len(fortunes)-1)])
+        return
 
     ###########################################################################
     # Infraction command
     ###########################################################################
     if message.content.startswith('$infraction'):
-        if message.author.id != jenid:
-            await message.channel.send("I don't take orders from you.")
-            return
-
-        taggedUser = message.mentions[0]
-
-        if str(taggedUser.id) in infractions:
-            infractions[str(taggedUser.id)] += 1
-        else:
-            infractions[str(taggedUser.id)] = 1
-
-        with open(infractfile, 'w') as outfile:
-            json.dump(infractions, outfile)
-
-        msg="" + taggedUser.name + " infractions: " + str(infractions[str(taggedUser.id)])
-        await message.channel.send(msg)
+        await do_infraction(message)
+        return
 
     ###########################################################################
     # Points command
     ###########################################################################
     if message.content.startswith('$points'):
-
-        if not str(message.author.id) in points:
-            points[str(message.author.id)] = 0
-
-        msg="" + message.author.name + " points: " + str(points[str(message.author.id)])
+        msg="" + message.author.name + " points: " + str(get_points(message.author.id))
         await message.channel.send(msg)
+        return
 
     ###########################################################################
     # Gatcha
     ###########################################################################
     if message.content.startswith('$roll'):
-
-        team = get_poketeam(message.author.id)
-
-        if len(team) == 0:
-            newpokeid = random.randint(1, 898)
-        elif len(team) >= 6:
-            await message.channel.send('You already have 6 pokemon you greedy bitch')
-            await message.channel.send('hint: $tothefarm <pokemon> to release')
-            return
-        else:
-            pts = get_points(message.author.id)
-            if pts < 10:
-                await message.channel.send('You have insufficient points (10 per roll): ' + str(pts))
-                return
-            add_points(message.author.id, -10)
-            newpokeid = random.randint(1, 898)
-
-        await message.channel.send(':game_die: R O L L I N G :game_die:')
-
-        newpoke = {}
-        newpoke['id'] = newpokeid
-        newpoke['name'] = pb.pokemon(newpokeid).name
-        newpoke['basepic'] = pb.SpriteResource('pokemon', newpokeid).url
-        team.append(newpoke)
-        set_poketeam(message.author.id, team)
-
-        log(message.author.name + ' new pokemon ' + newpoke['name'])
-
-        await message.channel.send('Welcome ' + newpoke['name'] + ' to your team!')
-        await message.channel.send(newpoke['basepic'])
+        await do_pokeroll(message)
+        return
 
     if message.content.startswith('$team'):
-
-        if len(message.mentions) > 0:
-            user = message.mentions[0];
-        else:
-            user = message.author
-
-        team = get_poketeam(user.id)
-
-        i = 0
-        secs = 0
-
-        for pokemon in team:
-            embed = poke_embed(pokemon)
-            await message.channel.send(embed=embed)
-        #await message.add_reaction('⬅')
-        #await message.add_reaction('➡')
+        await do_poketeam(message)
+        return
 
     if message.content.startswith('$tothefarm'):
-        words = message.content.split(" ")
-        if len(words) > 1:
-            # look for the pokemon send to the farm
-            team = get_poketeam(message.author.id)
-
-            if words[1] == "allthembitches":
-                await message.channel.send('Goodbye bitches!')
-                nberries = 0
-                for i in range(len(team)):
-                    nberries += random.randint(1, 10)
-                    await message.channel.send(team[i]['basepic'])
-                team = []
-                set_poketeam(message.author.id, team)
-                return
-
-            for i in range(len(team)):
-                if team[i]['name'] == words[1]:
-                    nberries = random.randint(1, 10)
-                    await message.channel.send('Goodbye ' + team[i]['name'] + '!')
-                    await message.channel.send('The farm sent you back ' + str(nberries) + ' ~~soylent chunks~~ berries')
-                    del team[i]
-                    add_berries(message.author.id, nberries)
-                    set_poketeam(message.author.id, team)
-                    return
-        await message.channel.send('Usage: $tothefarm <pokemon name/allthembitches>') 
+        await do_murder(message)
+        return
 
     if message.content.startswith('$berry'):
-        nberries = get_berries(message.author.id)
-        msg="" + message.author.name + " berries: " + str(nberries)
+        msg = message.author.name + " berries: " 
+        msg += str(get_berries(message.author.id))
         await message.channel.send(msg)
 
     if message.content.startswith('$slots'):
+        await do_slots(message)
+        return
 
+async def do_slots(message):
+
+    words = message.content.split(" ")
+    poorbitch='You have insufficient points (1 per spin): '
+    await message.channel.send(':game_die: S L O T S :game_die:')
+
+    nspins = 1
+
+    # If they provided a number that's the number of spins
+    # Max 10 spins at a time
+    try:
+        nspins = int(words[1]) 
+        if nspins > 10:
+            nspins = 10 
+        await message.channel.send('Spinning ' + str(nspins) + ' times')
+    except:
+        nspins = 1
+        await message.channel.send('NOT A NUMBER ')
+
+
+    while nspins > 0:
+        nspins -= 1
         pts = get_points(message.author.id)
-
         if pts < 1:
-            await message.channel.send('You have insufficient points (1 per spin): ' + str(pts))
+            await message.channel.send(poorbitch + str(pts))
             return
+        await pull_slot(message)
 
-        await message.channel.send(':game_die: S L O T S :game_die:')
+async def pull_slot(message):
 
-
-def pull_slot(message):
-    icons = [':fire:', ':leaves:', ':ocean:', ':cherries:', ':coin:']
-    ncols = 3
-    result = [None] * ncols
     pts = -1
-
+    ncols = 3
     string = ''
+    result = [None] * ncols
+    icons = [':fire:', ':leaves:', ':ocean:', ':cherries:', ':coin:']
 
     for i in range(0, ncols):
         result[i] = random.randint(0, len(icons)-1)
@@ -271,27 +340,31 @@ def pull_slot(message):
 
     max_icon = 0
     max_matches = 1
+    matches = 0
 
+    # Check for matches
     for i in range(0, len(icons)):
-        if result[i] == i:
+        matches = 0
+        for j in result:
+            if j == i:
+                matches += 1
+        if matches > max_matches:
+            max_icon = i
+            max_matches = matches
 
-    string = ''
-    if (result[0] == result[1]) and (result[0] == result[2]):
-        pts = (result[0] + 1) * 10
+    string=''
+
+    if max_matches == ncols:
+        pts = (max_icon + 1) * 10
         string = string + '\n    J A C K P O T    \n***' + str(pts) + '***'
-    elif result[0] == result[1] or result[0] == result[2]: 
-        pts = ((result[0] + 1) // 2) + 1
-        string = string + '\nSmall Winner!\n***' + str(pts) + '***'
-    elif result[1] == result[2]:
-        pts = ((result[1] + 1) // 2) + 1
+    elif max_matches > 1:
+        pts = ((max_icon + 1) // 2) + 1
         string = string + '\nSmall Winner!\n***' + str(pts) + '***'
     else:
         string = string + '\n***' + 'Try again' + '***' + ' :grimacing:'
-
+    
     await message.channel.send(string)
     add_points(message.author.id, pts)
-
-    
 
 def poke_embed(pokemon):
     embed = discord.Embed(title=pokemon['name'].capitalize())
@@ -300,7 +373,6 @@ def poke_embed(pokemon):
         pokemon['lvl'] = 1
     embed.add_field(name='Level', value=pokemon['lvl'], inline=True)
     return embed
-
 
 def get_berries(uid):
     if not str(uid) in berries:
@@ -329,8 +401,6 @@ def add_points(uid, pts):
 
     with open(pointsfile, 'w') as outfile:
         json.dump(points, outfile)
-
-    return points[str(uid)]
 
 #def set_pokemon(uid, team):
 #    pokemon[str(uid)] = team
